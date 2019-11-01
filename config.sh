cb667f77b8c6474cf63b376b732bb521e6f2bef7
Marty Sullivan <marty@martysullivan.com>
2019-11-01 20:00:55 +0000

    add batch roles

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-export APPLICATION=""
-export GITHUB_REPO=""
+export APPLICATION="ServerlessTemplate"
+export GITHUB_REPO="https://github.com/marty-sullivan/serverless-template.git"
 export ADMIN_EMAIL=""
 export ADMIN_PHONE=""
