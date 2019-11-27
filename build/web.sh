31ec725f28a705ab7c3e66c190a315e8e5ddfb09
Marty Sullivan <marty@martysullivan.com>
2019-11-27 18:50:42 +0000

    generate apigclient

--- build/web.sh
+++ build/web.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+API_ID=$(aws cloudformation describe-stacks \
+  --stack-name "$APPLICATION-$ENVIRONMENT" \
+  --query 'Stacks[0].Outputs[?OutputKey==`Api`].OutputValue' \
+  --output text)
+
+aws apigateway get-sdk \
+  --rest-api-id "$API_ID" \
+  --stage-name 'api' \
+  --sdk-type javascript \
+  $CODEBUILD_SRC_DIR/build/sdk.zip
+
+unzip \
+  $CODEBUILD_SRC_DIR/build/sdk.zip \
+  -d $CODEBUILD_SRC_DIR/web/
+
 WEB_BUCKET=$(aws cloudformation describe-stacks \
   --stack-name "$APPLICATION-$ENVIRONMENT" \
   --query 'Stacks[0].Outputs[?OutputKey==`WebBucket`].OutputValue' \
