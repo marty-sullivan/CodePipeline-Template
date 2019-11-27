5668f05753d97fba52d674af189b2b4f47de7679
Marty Sullivan <marty@martysullivan.com>
2019-11-27 06:59:01 +0000

    add web distribution

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 export APPLICATION="ServerlessTemplate"
-export DOMAIN_NAME="example.com"
+export DOMAIN_NAME="serverless-template.martysullivan.com"
 export GITHUB_REPO="https://github.com/marty-sullivan/serverless-template.git"
 export ADMIN_EMAIL=""
 export ADMIN_PHONE=""
