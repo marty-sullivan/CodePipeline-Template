823497f0206674280dad82e2da49e537440412a2
Marty Sullivan <marty@martysullivan.com>
2020-04-12 17:03:20 +0000

    fix cfn.sh

--- build/cfn.sh
+++ build/cfn.sh
@@ -5,7 +5,7 @@ STACK_NAME="$APPLICATION-$ENVIRONMENT"
 echo "Packaging CloudFormation Template..."
 
 aws cloudformation package \
-  --template-file $CODEBUILD_SRC_DIR/build/full.yml \
+  --template-file $CODEBUILD_SRC_DIR/build/root.yml \
   --s3-bucket $BUILD_BUCKET \
   --output-template-file $CODEBUILD_SRC_DIR/build/packaged.yml \
 
