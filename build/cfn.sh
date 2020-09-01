a58b6a5ee8de837370e111b44b170e431f9eabbe
Marty Sullivan <marty@martysullivan.com>
2020-09-01 19:24:11 +0000

    try codepipeline

--- build/cfn.sh
+++ build/cfn.sh
@@ -17,53 +17,54 @@ then
 
 fi
 
-echo "Deploying CloudFormation Template..."
+# echo "Deploying CloudFormation Template..."
 
-aws cloudformation deploy \
-  --stack-name $STACK_NAME \
-  --template-file $CODEBUILD_SRC_DIR/build/packaged.yml \
-  --s3-bucket $BUILD_BUCKET \
-  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    "Application=$APPLICATION" \
-    "Environment=$ENVIRONMENT" \
-    "DomainName=$DOMAIN_NAME" \
-    "GitHubRepo=$GITHUB_REPO" \
-    "AdminEmail=$ADMIN_EMAIL" \
-    "AdminPhone=$ADMIN_PHONE" \
-  --tags \
-    "Application=$APPLICATION" \
-    "Environment=$ENVIRONMENT" \
+# aws cloudformation deploy \
+#   --stack-name $STACK_NAME \
+#   --template-file $CODEBUILD_SRC_DIR/build/packaged.yml \
+#   --s3-bucket $BUILD_BUCKET \
+#   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+#   --parameter-overrides \
+#     "Application=$APPLICATION" \
+#     "Environment=$ENVIRONMENT" \
+#     "DomainName=$DOMAIN_NAME" \
+#     "GitHubOwner=$GITHUB_OWNER" \
+#     "GitHubRepo=$GITHUB_REPO" \
+#     "AlertEmail=$ALERT_EMAIL" \
+#     "AlertPhone=$ALERT_PHONE" \
+#   --tags \
+#     "Application=$APPLICATION" \
+#     "Environment=$ENVIRONMENT" \
 
-if [ $? != 0 ]
-then
+# if [ $? != 0 ]
+# then
 
-  echo "ERROR: Unable to deploy stack..."
-  exit 1
+#   echo "ERROR: Unable to deploy stack..."
+#   exit 1
 
-fi
+# fi
 
-if [ "$ENVIRONMENT" == "master" ]
-then
+# if [ "$ENVIRONMENT" == "master" ]
+# then
 
-  echo "Enabling Termination Protection for master branch stack: $STACK_NAME"
+#   echo "Enabling Termination Protection for master branch stack: $STACK_NAME"
   
-  aws cloudformation update-termination-protection \
-    --stack-name $STACK_NAME \
-    --enable-termination-protection > /dev/null
+#   aws cloudformation update-termination-protection \
+#     --stack-name $STACK_NAME \
+#     --enable-termination-protection > /dev/null
 
-fi
+# fi
 
-BUILD_PROJECT=$(aws cloudformation describe-stacks \
-  --stack-name "$STACK_NAME" \
-  --query 'Stacks[0].Outputs[?OutputKey==`BuildProject`].OutputValue' \
-  --output text)
+# BUILD_PROJECT=$(aws cloudformation describe-stacks \
+#   --stack-name "$STACK_NAME" \
+#   --query 'Stacks[0].Outputs[?OutputKey==`BuildProject`].OutputValue' \
+#   --output text)
 
-echo "Setting GitHub branch filter to '$ENVIRONMENT' for CodeBuild Project: $BUILD_PROJECT..."
+# echo "Setting GitHub branch filter to '$ENVIRONMENT' for CodeBuild Project: $BUILD_PROJECT..."
 
-aws codebuild update-webhook \
-  --project-name $BUILD_PROJECT \
-  --branch-filter $ENVIRONMENT \
-  --rotate-secret > /dev/null
+# aws codebuild update-webhook \
+#   --project-name $BUILD_PROJECT \
+#   --branch-filter $ENVIRONMENT \
+#   --rotate-secret > /dev/null
 
 echo "Done Building!"
